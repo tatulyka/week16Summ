2805eb007b5a29a7b3e499f0960f4d4439300023
Tatulyka <tatulyka@gmail.com>
2022-04-16 01:05:57 +0200

    done

--- script.js
+++ script.js
@@ -1,17 +1,36 @@
-
-
-    let sum;
-    let array = [];
-
+"use strict";
 function sumInput() {
-    for (i=0; i<array.length; i++) {
-      array[i] = +prompt('Введите пожалуйста число', +i);
-       if (!array[i]) break;
-       if (array[i] !== isNan) break;
-       if (array[i] === null) break;
-       sum += array[i];
+  let justDoIt = true,
+  number   = null,
+  nums = [],
+  sum = 0;
+    do{
+        let num = prompt("Введите число:");
+        
+        if(num === null || num.trim() === ''){ 
+          justDoIt = false;
+        }else if((num = parseFloat(num)) && !isNaN(num))           
+          nums.push(num); 
+        }      while(justDoIt);
+
+      
+      if(nums.length > 0){
+        nums.sort();  
+        alert(`Вы ввели ряд чисел: ${nums.join(', ')}`);
+      };    
+   
+    for (let i = 0; i < nums.length; i++)
+        { sum += nums[i];}
+        alert(`Сумма введенных чисел: ${sum}`);
     }
-    array.sort();
-    alert(array);
-    alert('cумма:' + sum);
-    }
+       
+
+
+
+
+
+           
+    
+
+    
+
